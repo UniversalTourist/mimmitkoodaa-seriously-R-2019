dcf3e74efb43d905db03beafafca0ca346ff3954
intael <carlos.valderrama@seriously.com>
2019-03-14 15:08:43 +0200

    Small changes

--- Preprocess.R
+++ Preprocess.R
@@ -1,11 +1,8 @@
 library(data.table)
 
-Dataset <- fread('./data/worked_data.csv',stringsAsFactors = T)
-Dataset[,Release.Date.Pretty :=as.Date(fasttime::fastPOSIXct(x = Release.Date.Pretty))]
-Dataset[,Last.Update.Pretty :=as.Date(fasttime::fastPOSIXct(x = Last.Update.Pretty))]
-Dataset[,Total.Sales := ifelse(is.na(Total.Sales) , 0 , Total.Sales)]
+df <- fread('./data/worked_data.csv',stringsAsFactors = T)
 
-Dataset %<>% mutate(
+df %<>% mutate(
   Release.Month = lubridate::month(Release.Date.Pretty , label = T),
   Release.Year = year(Release.Date.Pretty)
 )
--- exercises_draft.R
+++ exercises_draft.R
@@ -1,27 +1,38 @@
 options(scipen = 100)
 
 library(tidyverse)
-library(magrittr)
 library(scales)
-library(outliers)
 source("Preprocess.R")
-source("PlottingAssets.R")
 
-# Fiorst of all, check out the Dataframe!
-head(Dataset, 10)
+# First of all, check out the Dataframe!
+head(df, 10)
 
+# Base Theme (it will prettify the plots) ---------------------------------
+
+BasicTheme <- theme(
+  panel.grid.major = element_line(colour = "#F0F0F0"),
+  panel.grid.minor = element_line(colour = "#F0F0F0"),
+  panel.background = element_blank(),
+  text = element_text(
+    size = 10 ,
+    face = "bold" ,
+    vjust = 0.5 ,
+    hjust = 1
+  ),
+  axis.line = element_line(colour = "black")
+)
 
 # Project 1 - Critics Score VS User Score --------------------------------------------------------------------
 
-# Step 1-) Filter out missing values in the Critic.Score and User.Score variables
+# Step 1-) Filter out missing values in the Critic.Score and VGChartz.Score variables
 
 ScatterOutlierlessDS <-
-  Dataset %>% filter(!is.na(Dataset$Critic.Score), !is.na(Dataset$User.Score))
+  df %>% filter(!is.na(df$Critic.Score), !is.na(df$VGChartz.Score))
 
-# Step 2-) Use the geom_point() layer to generate a scatterplot that displays Critic.Score VS User.Score
+# Step 2-) Use the geom_point() layer to generate a scatterplot that displays Critic.Score VS VGChartz.Score
 
 Plot <-
-  ggplot(data = ScatterOutlierlessDS , aes(x = Critic.Score , y = User.Score)) +
+  ggplot(data = ScatterOutlierlessDS , aes(x = Critic.Score , y = VGChartz.Score)) +
   geom_point()
 
 # Step 3-) Use the labs layer to provide a Title, a Subtitle and x-y labels to the plot
@@ -29,7 +40,7 @@ Plot <-
 Plot <- Plot +
   labs(title = "Total Videogames Sales VS User Scores",
        x = "Critic Scores (0-10)",
-       y = "Users Scores (0-10)")
+       y = "VG Chartz Scores (0-10)")
 
 # Step 4-) Apply our Basic Theme to improve the layout
 
@@ -49,9 +60,9 @@ Plot
 
 # Step 1-) Get the Total Sales in 2018 broken down by Month
 
-SalesByMonth <- Dataset %>% filter(Release.Year == 2018) %>%
+SalesByMonth <- df %>% filter(Release.Year == 2018) %>%
   group_by(Release.Month) %>%
-  summarise(Sales = sum(Total.Sales , na.rm = T))
+  summarise(Sales = sum(Total.Sales , na.rm = TRUE))
 
 # Step 2-) Use the geom_line() layer to generate a line plot of Total Sales by Month
 
@@ -64,7 +75,7 @@ Plot <-
 # Step 3-) Provide a nicer format for the y-axis values (comma separated thousands and € sign)
 
 Plot <- Plot +
-  scale_y_continuous(labels = MoneyAxisFormat)
+  scale_y_continuous(labels = scales::number_format(big.mark = ","))
 
 # Step 4-) Use the labs layer to provide a Title, a Subtitle and x-y labels to the plot
 
@@ -72,7 +83,7 @@ Plot <- Plot + labs(
   title = "Total Videogames Sales By Month",
   subtitle = "Videogames released during 2018",
   x = "Month",
-  y = "Sales"
+  y = "Total Sales"
 )  +
   BasicTheme
 
@@ -95,7 +106,7 @@ Plot
 
 # Step 1-) Get The Top 3 Consoles in terms of amount of released games in 2018 
 
-Top5Consoles <- Dataset %>% filter(Release.Year == 2018) %>%
+Top5Consoles <- df %>% filter(Release.Year == 2018) %>%
   group_by(Console) %>%
   summarise(Releases = n()) %>%
   arrange(-Releases) %>%
@@ -105,9 +116,9 @@ Top5Consoles <- Dataset %>% filter(Release.Year == 2018) %>%
 # Step 2-) Compute the Total Sales of 2018 of the Top 5 Consoles in terms of the amount of released games in 2018 broken down by Console and Relase Month
 
 SalesByMonthAndDev <-
-  Dataset %>% filter(Release.Year == 2018, Console %in% Top5Consoles$Console) %>%
+  df %>% filter(Release.Year == 2018, Console %in% Top5Consoles$Console) %>%
   group_by(Release.Month , Console) %>%
-  summarise(Sales = sum(Total.Sales))
+  summarise(Sales = sum(Total.Sales , na.rm = TRUE))
 
 # Step 3-) Use the geom_line() layer to generate a line plot of Total Sales broken down by Month (in the X axis) and Console (colors and shapes)
 
@@ -123,7 +134,7 @@ Plot <- ggplot(data = SalesByMonthAndDev ,
 
 # Step 4-) Provide a nicer format for the y-axis values (comma separated thousands and € sign)
 
-Plot <- Plot + scale_y_continuous(labels = MoneyAxisFormat)
+Plot <- Plot + scale_y_continuous(labels = scales::number_format(big.mark = ","))
 
 # Step 5-) Use the labs layer to provide a Title, a Subtitle and x-y labels to the plot
 
@@ -131,7 +142,7 @@ Plot <- Plot +  labs(
     title = "Total Videogames Sales By Month broken down by top 5 game releaser Consoles",
     subtitle = "Videogames released during 2018",
     x = "Month",
-    y = "Sales"
+    y = "Total Sales"
   )
 
 # Step 6-) Apply our Basic Theme to improve the layout
@@ -148,28 +159,28 @@ Plot
 
 # Step 1-) Get the Top 5 Developers in terms of Total Sales in 2018
 
-Top5Developers <- Dataset %>% filter(Release.Year == 2018) %>%
+Top5Developers <- df %>% filter(Release.Year == 2018) %>%
   group_by(Developer) %>%
-  summarise(TotalSales = sum(Total.Sales)) %>%
+  summarise(TotalSales = sum(Total.Sales, na.rm = TRUE)) %>%
   arrange(-TotalSales) %>%
   head(5) %>%
   select(Developer)
 
 # Step 2-) Get the Top 5 Consoles in terms of Total Sales in 2018
 
-Top5Consoles <- Dataset %>% filter(Release.Year == 2018) %>%
+Top5Consoles <- df %>% filter(Release.Year == 2018) %>%
   group_by(Console) %>%
-  summarise(TotalSales = sum(Total.Sales)) %>%
+  summarise(TotalSales = sum(Total.Sales, na.rm = TRUE)) %>%
   arrange(-TotalSales) %>%
   head(5) %>%
   select(Console)
 
 # Step 3-) Get the total Sales by Developer and Console of the Top 5 Developers and Consoles
 
-SalesByDeveloper <- Dataset %>% 
+SalesByDeveloper <- df %>% 
   filter(Developer %in% Top5Developers$Developer , Console %in% Top5Consoles$Console) %>% 
   group_by(Developer,Console) %>%
-  summarise("Total Sales" = sum(Total.Sales)) %>% 
+  summarise("Total Sales" = sum(Total.Sales, na.rm = TRUE)) %>% 
   arrange(-`Total Sales`)
 
 # Step 4-) Use the geom_bar() layer to generate a bar plot of Total Sales broken down by Developer (in the X axis) and Console (colors)
@@ -178,21 +189,20 @@ Plot <- ggplot(data = SalesByDeveloper ,
                aes(
                  x = Developer ,
                  y = `Total Sales` ,
-                 fill = Console ,
-                 group = Console
+                 fill = Console
                )) +
   geom_bar(stat = "identity" , position = 'dodge')
 
 # Step 5-) Provide a nicer format for the y-axis values (comma separated thousands and € sign)
 
-Plot <- Plot + scale_y_continuous(labels = MoneyAxisFormat)
+Plot <- Plot + scale_y_continuous(labels = scales::number_format(big.mark = ","))
 
 # Step 6-) Use the labs layer to provide a Title, a Subtitle and x-y labels to the plot
 
 Plot <- Plot +
-  labs(title = "Total Videogames Sales by Developer and Console",
+  labs(title = "Total Videogames Sales by Top 5 Developers and Top 5 Consoles in 2018",
        x = "Developer",
-       y = "Total Sales (€)")
+       y = "Total Sales")
 
 # Step 7-) Apply our Basic Theme to improve the layout
 
